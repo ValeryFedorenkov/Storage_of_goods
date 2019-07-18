17de57e49d34c6383ee1397866c004f36ec93947
Valery <52780416+ValeryFedorenkov@users.noreply.github.com>
2019-07-18 19:38:45 +0100

    Add files via upload

---
+++ Valery_Fedorenkov_6/Valery_Fedorenkov_6/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace Valery_Fedorenkov_6
+{
+    public partial class Form1 : Form
+    {
+        public GoodsList goods = new GoodsList();
+        public Form1()
+        {
+            InitializeComponent();
+            goods.Add(new Goods("Pizza", 20, 10));
+            dataGridView1.DataSource = new BindingSource(goods, null);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = goods;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Goods g = new Goods(textBox1.Text, int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            goods.Add(g);
+            dataGridView1.DataSource = goods;
+            (BindingContext[goods] as CurrencyManager).Refresh();
+        }
+    }
+}
---
+++ Valery_Fedorenkov_6/Valery_Fedorenkov_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace Valery_Fedorenkov_6
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    public class Goods
+    {
+        public string Type { get; set; }   
+        public int Cost { get; set; }
+        public int Quantity { get; set; }
+        public Goods(string ProdName, int cost, int quantity)
+        {
+            Type = ProdName;
+            Cost = cost;
+            Quantity = quantity;
+        }
+    }
+    public class GoodsList : ArrayList, ITypedList
+    {
+        public PropertyDescriptorCollection
+            GetItemProperties(PropertyDescriptor[] listAccessors)
+        {
+            return TypeDescriptor.GetProperties(typeof(Goods));
+        }
+
+        public string GetListName(PropertyDescriptor[] listAccessors)
+        {
+            return "GoodsList";
+        }
+    }
+}
